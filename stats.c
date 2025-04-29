8f231cf9289262fa6a2e553c7ec9c40bec420510
Maya <maya.f.dunlap@gmail.com>
2025-04-29 10:00:03 -0600

    Adding code outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -9,20 +9,31 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c 
+ * @brief Calculates the statistics of a dataset and prints the dataset's
+ *        content and statistics to the console.
  *
- * <Add Extended Description Here>
+ * This application takes a dataset of unsigned char elements and performs
+ * statistical analytics (median, mean, minimum, and maximum) on this dataset.
+ * The dataset and its analytics can also be printed to the console.
+ * 
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Maya F. Dunlap
+ * @date 29 April 2025
  *
  */
 
 
-
 #include <stdio.h>
 #include "stats.h"
+
+void print_statistics( unsigned char* array_ptr, unsigned int size );
+void print_array( unsigned char* array_ptr, unsigned int size );
+unsigned char find_median( unsigned char* array_ptr, unsigned int size );
+unsigned char find_mean( unsigned char* array_ptr, unsigned int size );
+unsigned char find_maximum( unsigned char* array_ptr, unsigned int size );
+unsigned char find_minimum( unsigned char* array_ptr, unsigned int size );
+void sort_array( unsigned char* array_ptr, unsigned int size );
 
 /* Size of the Data Set */
 #define SIZE (40)
@@ -35,9 +46,114 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  /* Other Variable Declarations */
+
+  /* Statistics and Printing Functions */
 
 }
 
-/* Add other Implementation File Code Here */
+/* Implementation File Code */
+
+/******************************************************************************
+ * Function: print_statistics
+ * Description:
+ *     This function prints the statistics (median, mean, mimimum, and maximum)
+ *     of a data set.
+ * Parameters:
+ *     unsigned char * array_ptr: Pointer to a dataset
+ *     unsigned char        size: Number of items in the dataset
+ * Return: 
+ *     None.
+ *****************************************************************************/
+void print_statistics( unsigned char* array_ptr, unsigned int size ){
+
+}
+
+/******************************************************************************
+ * Function: print_array
+ * Description:
+ *     This function prints the values of a dataset.
+ * Parameters:
+ *     unsigned char * array_ptr: Pointer to a dataset
+ *     unsigned char        size: Number of items in the dataset
+ * Return: 
+ *     None.
+ *****************************************************************************/
+void print_array( unsigned char* array_ptr, unsigned int size ){
+
+}
+
+/******************************************************************************
+ * Function: find_median
+ * Description:
+ *     This function takes a set of numbers and performs finds the median of
+ *     of the dataset.
+ * Parameters:
+ *     unsigned char * array_ptr: Pointer to a data set
+ *     unsigned char        size: Number of items in data set
+ * Return: 
+ *     Median of the dataset.
+ *****************************************************************************/
+unsigned char find_median( unsigned char* array_ptr, unsigned int size ){
+
+}
+
+/******************************************************************************
+ * Function: find_mean
+ * Description:
+ *     This function takes a set of numbers and performs finds the average of
+ *     of the set.
+ * Parameters:
+ *     unsigned char * array_ptr: Pointer to a data set
+ *     unsigned char        size: Number of items in data set
+ * Return: 
+ *     Average of the dataset.
+ *****************************************************************************/
+unsigned char find_mean( unsigned char* array_ptr, unsigned int size ){
+
+}
+
+/******************************************************************************
+ * Function: find_maximum
+ * Description:
+ *     This function takes a set of numbers and performs finds the minimum
+ *     value of the set.
+ * Parameters:
+ *     unsigned char * array_ptr: Pointer to a data set
+ *     unsigned char        size: Number of items in data set
+ * Return: 
+ *     Minimum value of the dataset.
+ *****************************************************************************/
+unsigned char find_maximum( unsigned char* array_ptr, unsigned int size ){
+
+}
+
+/******************************************************************************
+ * Function: find_minimum
+ * Description:
+ *     This function takes a set of numbers and performs finds the maximum
+ *     value of the set.
+ * Parameters:
+ *     unsigned char * array_ptr: Pointer to a data set
+ *     unsigned char        size: Number of items in data set
+ * Return: 
+ *     Maximum value of the dataset.
+ *****************************************************************************/
+unsigned char find_minimum( unsigned char* array_ptr, unsigned int size ){
+
+}
+
+/******************************************************************************
+ * Function: sort_array
+ * Description:
+ *     This function takes a set of numbers and reorders the items in the
+ *     dataset from largest to smallest.
+ * Parameters:
+ *     unsigned char * array_ptr: Pointer to a data set
+ *     unsigned char        size: Number of items in data set
+ * Return: 
+ *     None.
+ *****************************************************************************/
+void sort_array( unsigned char* array_ptr, unsigned int size ){
+
+}
--- stats.h
+++ stats.h
@@ -9,32 +9,134 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h 
+ * @brief Header file which provides functions to calculate the statistics of a
+ *        dataset and prints the dataset's contents and statistics to the
+ *        console. 
  *
- * <Add Extended Description Here>
+ * This application takes a dataset of an unsigned char elements and performs
+ * statistical analytics (median, mean, minimum, and maximum) on this dataset.
+ * The dataset and its analytics can also be printed to the console.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Maya F. Dunlap
+ * @date 29 April 2025
  *
  */
+
+
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Prints the statistics of an unsigned char data array
  *
- * <Add Extended Description Here>
+ * This function takes a pointer to an n-element unsigned char data array and an
+ * unsigned integer which represents the size of the data array. The function then
+ * prints the median, mean, minimum, and maximum values of the data array. There
+ * is no returned value.
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param array_ptr The pointer to an n-element unsigned char data array
+ * @param size The size (unsigned integer) of the data array
  *
- * @return <Add Return Informaiton here>
+ * @return None
  */
+void print_statistics( unsigned char* array_ptr, unsigned int size );
 
+
+/**
+ * @brief Prints the elements of an unsigned char data array
+ *
+ * This function takes a pointer to an n-element unsigned char data array and an
+ * unsigned integere which represents the size of the data array. The function then
+ * prints the values of the data array to the console. There is no returned value.
+ *
+ * @param array_ptr The pointer to an n-element unsigned char data array
+ * @param size The size (unsigned integer) of the data array
+ *
+ * @return None
+ */
+void print_array( unsigned char* array_ptr, unsigned int size );
+
+
+/**
+ * @brief Calculates the median value of the elements of an unsigned char data array
+ *
+ * This function takes a pointer to an n-element unsigned char data array and an
+ * unsigned integer which represents the size of the data array. The function then
+ * finds and returns the median value (unsigned char) of all the elements in the
+ * array rounded down to the nearest integer.
+ *
+ * @param array_ptr The pointer to an n-element unsigned char data array
+ * @param size The size (unsigned integer) of the data array
+ *
+ * @return The median value (unsigned char) of the elements in the data array
+ */
+unsigned char find_median( unsigned char* array_ptr, unsigned int size );
+
+
+/**
+ * @brief Calcuates the average value of the elements of an unsigned char data array
+ *
+ * This function takes a pointer to an n-element unsigned char data array and an
+ * unsigned integer which represents the size of the data array. The function then
+ * finds and returns the average value (unsigned char) of all the elements in the
+ * array rounded down to the nearest integer.
+ *
+ * @param array_ptr The pointer to an n-element unsigned char data array
+ * @param size The size (unsigned integer) of the data array
+ *
+ * @return The average value (unsigned char) of the elements in the data array
+ */
+unsigned char find_mean( unsigned char* array_ptr, unsigned int size );
+
+
+/**
+ * @brief Finds the element of an unsigned char data array with the largest value
+ *
+ * This function takes a pointer to an n-element unsigned char data array and
+ * an unsigned integer which represents the size of the data array. The function
+ * then finds and returns the largest value (unsigned char) found within the
+ * data array.
+ *
+ * @param array_ptr The pointer to an n-element unsigned char data array
+ * @param size The size (unsigned integer) of the data array
+ *
+ * @return The largest unsigned char value in the data array
+ */
+unsigned char find_maximum( unsigned char* array_ptr, unsigned int size );
+
+
+/**
+ * @brief Finds the element of an unsigned char data array with the smallest value
+ *
+ * This function takes a pointer to an n-element unsigned char data array and
+ * an unsigned integer which represents the size of the data array. The function
+ * then finds and returns the smallest value (unsigned char) found within the
+ * data array.
+ *
+ * @param array_ptr The pointer to an n-element unsigned char data array
+ * @param size The size (unsigned integer) of the data array
+ *
+ * @return The smallest unsigned char value in the data array
+ */
+unsigned char find_minimum( unsigned char* array_ptr, unsigned int size );
+
+
+/**
+ * @brief Sort the elements in a unsigned char data array from largest to smallest
+ *
+ * This function takes a pointer to an n-element unsigned char data array and
+ * an unsigned integer which represents the size of the data array. The function
+ * then reorders the elements of the original array. The largest element in the
+ * data set is the first element in the array and the smallest element in the
+ * data set is the last element in the array. There is no returned value.
+ *
+ * @param array_ptr The pointer to an n-element unsigned char data array
+ * @param size The size (unsigned integer) of the data array
+ *
+ * @return None
+ */
+void sort_array( unsigned char* array_ptr, unsigned int size );
 
 #endif /* __STATS_H__ */
