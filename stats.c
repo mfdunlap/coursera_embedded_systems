215f894ab933c08502b6e8d145d93e1d62b0d518
Maya <maya.f.dunlap@gmail.com>
2025-04-29 13:01:19 -0600

    Adding Statistics and Printing Features to c1m1

--- stats.c
+++ stats.c
@@ -41,14 +41,33 @@ void sort_array( unsigned char* array_ptr, unsigned int size );
 void main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
+                              114,  88,  45,  76, 123,  87,  25,  23,
+                              200, 122, 150,  90,  92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations */
+  unsigned char *array_ptr = test;
 
   /* Statistics and Printing Functions */
+  printf( "UNSORTED ARRAY:\n" );
+  print_array( array_ptr, SIZE );
+
+  printf( "\nDATASET STATISTICS (Print Stats Function):\n" );
+  print_statistics( array_ptr, SIZE );
+
+  printf( "\nDATASET STATISTICS (Individual Stat Functions):\n" );
+  unsigned char median = find_median( array_ptr, SIZE );
+  printf( "Median:  %d\n", median );
+
+  unsigned char mean = find_mean( array_ptr, SIZE );
+  printf( "Mean:    %d\n", mean );
+
+  unsigned char minimum = find_minimum( array_ptr, SIZE );
+  printf( "Minimum: %d\n", minimum );
+
+  unsigned char maximum = find_maximum( array_ptr, SIZE );
+  printf( "Maximum: %d\n", maximum );
 
 }
 
@@ -66,7 +85,15 @@ void main() {
  *     None.
  *****************************************************************************/
 void print_statistics( unsigned char* array_ptr, unsigned int size ){
+  unsigned char median = find_median( array_ptr, size );
+  unsigned char mean = find_mean( array_ptr, size );
+  unsigned char minimum = find_minimum( array_ptr, size );
+  unsigned char maximum = find_maximum( array_ptr, size );
 
+  printf( "Median:  %d\n", median );
+  printf( "Mean:    %d\n", mean );
+  printf( "Minimum: %d\n", minimum );
+  printf( "Maximum: %d\n", maximum );
 }
 
 /******************************************************************************
@@ -80,6 +107,10 @@ void print_statistics( unsigned char* array_ptr, unsigned int size ){
  *     None.
  *****************************************************************************/
 void print_array( unsigned char* array_ptr, unsigned int size ){
+  // Iterate through the aray and print each element line
+  for ( int i = 0; i < size; i++ ){
+    printf( "item[%d]: %d\n", i, array_ptr[i] );
+  }
 
 }
 
@@ -95,7 +126,20 @@ void print_array( unsigned char* array_ptr, unsigned int size ){
  *     Median of the dataset.
  *****************************************************************************/
 unsigned char find_median( unsigned char* array_ptr, unsigned int size ){
+  unsigned char median;
+  
+  // Sort array ( largest --> smallest )
+  sort_array( array_ptr, size );
 
+  // Calcuate median
+  if( size % 2 == 0 ){  // Even number of array items
+    median = ( array_ptr[( size / 2 ) - 1] + array_ptr[size / 2] ) / 2;
+  }
+  else{  // Odd number of array items
+    median = array_ptr[size / 2];
+  }
+
+  return median;
 }
 
 /******************************************************************************
@@ -110,7 +154,14 @@ unsigned char find_median( unsigned char* array_ptr, unsigned int size ){
  *     Average of the dataset.
  *****************************************************************************/
 unsigned char find_mean( unsigned char* array_ptr, unsigned int size ){
+  // Calculate dataset total
+  unsigned int total = 0;
+  
+  for( int i = 0; i < size; i++ ){
+    total += array_ptr[i];
+  }
 
+  return total / size;
 }
 
 /******************************************************************************
@@ -125,7 +176,11 @@ unsigned char find_mean( unsigned char* array_ptr, unsigned int size ){
  *     Minimum value of the dataset.
  *****************************************************************************/
 unsigned char find_maximum( unsigned char* array_ptr, unsigned int size ){
+  // Sort array (largest --> smallest)
+  sort_array( array_ptr, size );
 
+  // Get first element in sorted array
+  return array_ptr[0];
 }
 
 /******************************************************************************
@@ -140,7 +195,11 @@ unsigned char find_maximum( unsigned char* array_ptr, unsigned int size ){
  *     Maximum value of the dataset.
  *****************************************************************************/
 unsigned char find_minimum( unsigned char* array_ptr, unsigned int size ){
-
+  // Sort array (largest --> smallest)
+  sort_array( array_ptr, size );
+  
+  // Get last element in sorted array
+  return array_ptr[size - 1];
 }
 
 /******************************************************************************
@@ -155,5 +214,19 @@ unsigned char find_minimum( unsigned char* array_ptr, unsigned int size ){
  *     None.
  *****************************************************************************/
 void sort_array( unsigned char* array_ptr, unsigned int size ){
+  // Insertion sort algorithm (descending)
+  for( int i = 1; i < size; i++ ){
+    unsigned char key = array_ptr[i];
+    int j = i - 1;
 
+    // Move elements of array[0...i-1] that are lesser than key to one
+    // position ahead of current position.
+    while( j >= 0 && array_ptr[j] < key ){
+      array_ptr[j + 1] = array_ptr[j];
+      j--;
+    }
+
+    // Move key to correct position
+    array_ptr[j + 1] = key;
+  }
 }
